44d1e7f4b106f07ca858e58055ffaca0adc8b85e
Ben <bhawkyard1@gmail.com>
2022-04-01 10:04:17 +0100

    import skipton

--- src/ledger/ingestion/skipton.py
+++ src/ledger/ingestion/skipton.py
@@ -8,7 +8,7 @@ class Skipton(Ingestor):
     def ingest(self):
         transactions = []
         for idx, line in enumerate(reversed(self.data[1:])):
-            print(repr(line))
+            print(repr(line),[x for x in line.split("\t") if x])
             datestr, desc, pay_in, pay_out, balance = [x for x in line.split("\t") if x]
             day, month, year = datestr.split("/")
             amount = self.to_pence(pay_in) if pay_in else -self.to_pence(pay_out)
